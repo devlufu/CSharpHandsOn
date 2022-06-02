6ebde3433495db7153cd4d0fc9b922986790fc5c
tryhard67 <bhavishpoojary58@gmail.com>
2022-06-02 21:45:59 +0530

    session-2 commit

---
+++ Bhavish/session-2/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+     class _Employee
+    {
+        public string _name { get; set; }   
+        public int _age { get; set; }
+        public double _salary { get; set; }
+        public string _address1 { get; set; }
+        public string _address2 { get; set; }
+        public _Employee(string name, int age, double salary, string address1, string address2)
+        {
+            _name = name;
+            _age = age;
+            _salary = salary;
+            _address1 = address1;
+            _address2 = address2;  
+        }
+        public void display()
+        {
+            Console.WriteLine(_name + " of age " +_age);
+        }
+
+    }
+}
---
+++ Bhavish/session-2/main.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Employee;
+
+    _Employee employee1 = new _Employee("alan",20,30000.0,"Jyothinagar, Chikkamagalur","Sakaleshpur, Hassan");
+
+    _Employee employee2 = new _Employee("jose",23,45000.0,"Rajajinagar, Bangalore","Kuvempunagar, Mandya");
+
+    employee1.display();
+    employee2.display();
+    Console.ReadLine();
